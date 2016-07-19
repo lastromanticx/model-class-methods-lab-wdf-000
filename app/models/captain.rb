cdbc3f895ad1ab79545f0442429e29098d0ff6df
lastromanticx <giladbarkan@live.com>
2016-07-18 22:56:42 -0400

    still working

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,7 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Boat.joins(:classifications).joins(:captain).where("classifications.name = 'Catamaran'").select("captains.*")
+  end
 end
